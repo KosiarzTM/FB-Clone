a123046025d00cadb714f466c469a68b66d033c2
s.kowalski <s.kowalski@gloo.pl>
2020-12-25 19:39:27 +0100

    xx

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -30,8 +30,8 @@ class Database extends \CodeIgniter\Database\Config
 	 * @var array
 	 */
 	public $default = [
-		'DSN'      => '',
-		'hostname' => 'ec2-46-137-100-204.eu-west-1.compute.amazonaws.com',
+		'DSN'      => 'pgsql:host=localhost;port=5432;dbname=d7lumcus8b7jgo',
+		'hostname' => 'localhost',
 		'username' => 'yinweuigvieneh',
 		'password' => '0b087166bb85d6d2c2a88e29ad14eb8941844e3d4627843efd566faa4e77cdf2',
 		'database' => 'd7lumcus8b7jgo',
