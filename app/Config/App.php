e78067b672bc37470acd1255d65cd903cd2cf8aa
s.kowalski <s.kowalski@gloo.pl>
2020-12-26 19:41:50 +0100

    x

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'https://fb-clonee.herokuapp.com/public/';
+	public $baseURL = '';
 
 	/*
 	|--------------------------------------------------------------------------
